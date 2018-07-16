a50ff87e8fc8a0d20da6ddd0058257aa4a70e925
dunkthelunk <prudhvi.porandla@icloud.com>
2018-07-16 12:53:48 +0530

    remove intermediate python

--- src/query/learn/proto.py
+++ src/query/learn/proto.py
@@ -1,4 +1,4 @@
-INTEGER, PLUS, MINUS, EOF = 'INTEGER', 'PLUS', 'MINUS', 'EOF'
+INTEGER, PLUS, MINUS, DIV, MULT, EOF, LPAREN, RPAREN = 'INTEGER', 'PLUS', 'MINUS', 'DIV', 'MULT', 'EOF', 'LPAREN', 'RPAREN'
 
 class Token:
     def __init__(self, typee, value):
@@ -11,17 +11,15 @@ class Token:
     def __repr__(self):
         return self.__str__()
 
-class Interpreter():
-    def __init__(self, text):
+class Lexer:
+    def __init__(self,text):
         self.text = text
         self.pos = 0
         self.current_char = self.text[self.pos]
-        self.current_token = None
-    
-    #Lexer
+
     def error(self):
-        raise Exception('Invalid syntax')
-    
+        raise Exception('Invalid character')
+
     def advance(self):
         self.pos += 1
         if self.pos > len(self.text)-1:
@@ -41,67 +39,102 @@ class Interpreter():
         return int(result)
 
     def get_next_token(self):
-        
         if self.current_char is None:
             return Token(EOF, None)
-       
+
         if self.current_char.isspace():
             self.skip_whitespace()
-        
+
         if self.current_char.isdigit():
             return Token(INTEGER, self.integer())
-        
+
         if self.current_char == '+':
             self.advance()
             return Token(PLUS, '+')
-        
+
         if self.current_char == '-':
             self.advance()
             return Token(MINUS, '-')
+
+        if self.current_char == '(':
+            self.advance()
+            return Token(LPAREN, '(')
+
+        if self.current_char == ')':
+            self.advance()
+            return Token(RPAREN, ')')
         
+        if self.current_char == '/':
+            self.advance()
+            return Token(DIV, '/')
+
+        if self.current_char == '*':
+            self.advance()
+            return Token(MULT, '*')
         self.error()
 
-    #Parser/Interpreter
+class Parser:
+    def __init__(self, lexer):
+        self.lexer = lexer
+        self.current_token = self.lexer.get_next_token()
+
+    def error(self):
+        raise Exception('Invalid syntax')
 
     def eat(self, token_type):
         if self.current_token.type == token_type:
-            self.current_token = self.get_next_token()
+            self.current_token = self.lexer.get_next_token()
         else:
             self.error()
-    
-    def term(self):
+
+    def factor(self):
         token = self.current_token
-        self.eat(INTEGER)
-        return token.value
+        if token.type is INTEGER:
+            self.eat(INTEGER)
+        elif token.type is LPAREN:
+            self.eat(LPAREN)
+            self.expr()
+            self.eat(RPAREN)
+        else:
+            self.error()
+
+    def term(self):
+        self.factor()
+
+        while self.current_token.type in (DIV, MULT):
+            token = self.current_token
+            if token.type is DIV:
+                self.eat(DIV)
+                self.factor()
+            elif token.type is MULT:
+                self.eat(MULT)
+                self.factor()
 
     def expr(self):
-        self.current_token = self.get_next_token()
-        result = self.term()
+        self.term()
 
-        while self.current_token.type in (MINUS, PLUS):
+        while self.current_token.type in (PLUS, MINUS):
             token = self.current_token
-            if token.type == PLUS:
+            if token.type is PLUS:
                 self.eat(PLUS)
-                result+=self.term()
-            else:
+                self.term()
+            elif token.type is MINUS:
                 self.eat(MINUS)
-                result-=self.term()
-        return result
+                self.term()
+
+    def parse(self):
+        self.expr()
 
 def main():
     while True:
         try:
-            # To run under Python3 replace 'raw_input' call
-            # with 'input'
             text = input('calc> ')
         except EOFError:
             break
         if not text:
             continue
-        interpreter = Interpreter(text)
-        result = interpreter.expr()
-        print(result)
-
+        parser = Parser(Lexer(text))
+        parser.parse()
 
 if __name__ == '__main__':
     main()
