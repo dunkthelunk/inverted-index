55c7dd0b87080acb5eec4ddd344de3c88b801456
dunkthelunk <prudhvi.porandla@icloud.com>
2018-07-26 00:35:02 +0530

    cleanup

--- include/IntSequence.h
+++
@@ -1,9 +0,0 @@
-class IntSequence {
-private:
-  int Current;
-
-public:
-  IntSequence();
-  IntSequence(int StartFrom);
-  int next();
-}
--- src/IntSequence.cpp
+++
@@ -1,7 +0,0 @@
-#include "IntSequence.h"
-
-IntSequence::IntSequence() : Current{0} {}
-
-IntSequence::IntSequence(int StartFrom) : Current{StartFrom} {}
-
-IntSequence::next() { return ++Current; }
